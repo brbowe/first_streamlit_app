9d00447814d979e27e6685e057eb7b46c4dbb465
brbowe <123421753+brbowe@users.noreply.github.com>
2023-01-23 17:11:16 -0600

    Update streamlit_app.py
    fixed syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ streamlit.text ('🥦 Kale, Spinich & Rocket Smoothie')
 streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text ('🥑🍞Avacado Toast')
 
-streamlit.header(🍓🍉'Build Your Own Fruit Smoothie'🍎🍏)
+streamlit.header('🍓🍉Build Your Own Fruit Smoothie🍎🍏')
 
