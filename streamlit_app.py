8343d6295e882ca8cd65ab8e256a2cf2ce35229c
brbowe <123421753+brbowe@users.noreply.github.com>
2023-01-23 16:49:33 -0600

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(:blueberry:,'Omega 3 & Blueberry Oatmeal')
-streamlit.text(:kale:,'Kale, Spinich & Rocket Smoothie')
-streamlit.text(:egg:,'Hard-Boiled Free-Range Egg')
-streamlit.text(:avacado:,:toast:,'Avacado Toast')
+streamlit.text(:blueberry: 'Omega 3 & Blueberry Oatmeal')
+streamlit.text(:kale: 'Kale, Spinich & Rocket Smoothie')
+streamlit.text(:egg: 'Hard-Boiled Free-Range Egg')
+streamlit.text(:avacado: :toast: 'Avacado Toast')
 
