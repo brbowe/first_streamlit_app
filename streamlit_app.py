8883a00e4cfe56758256352cafe0bfc6cf77013f
brbowe <123421753+brbowe@users.noreply.github.com>
2023-01-23 16:42:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.txt('Kale, Spinich & Rocket Smoothie')
-streamlit.txt('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinich & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
