1386fcf0a53d87c27523fe893716f2801e2ce7b2
brbowe <123421753+brbowe@users.noreply.github.com>
2023-01-23 16:59:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text (:blueberries: 'Omega 3 & Blueberry Oatmeal')
-streamlit.text (:kale:)('Kale, Spinich & Rocket Smoothie')
-streamlit.text (:egg:)('Hard-Boiled Free-Range Egg')
-streamlit.text (:avacado: :toast:) ('Avacado Toast')
+streamlit.text (':blueberries: Omega 3 & Blueberry Oatmeal')
+streamlit.text (':leafy_green: Kale, Spinich & Rocket Smoothie')
+streamlit.text (':chicken: Hard-Boiled Free-Range Egg')
+streamlit.text (':avacado: :toast: Avacado Toast')
 
