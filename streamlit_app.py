ead51e2ec16b37f9c69c89daf01dacbd6af3196a
brbowe <123421753+brbowe@users.noreply.github.com>
2023-01-23 17:08:44 -0600

    Update streamlit_app.py
    fix emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 import streamlit
-import emoji
 
 
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text (':blueberries: Omega 3 & Blueberry Oatmeal')
-streamlit.text (':leafy_green: Kale, Spinich & Rocket Smoothie')
-streamlit.text (':chicken: Hard-Boiled Free-Range Egg')
-streamlit.text (':avacado: :toast: Avacado Toast')
+streamlit.text ('🥄 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥦 Kale, Spinich & Rocket Smoothie')
+streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text ('🥑🍞Avacado Toast')
 
