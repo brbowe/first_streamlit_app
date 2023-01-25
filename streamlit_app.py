9e0b93aaa3f0028bde0d419273676c5bb0c1bd92
brbowe <123421753+brbowe@users.noreply.github.com>
2023-01-25 11:18:04 -0600

    Update streamlit_app.py
    fix panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ streamlit.text ('🥑🍞Avacado Toast')
 streamlit.header('🍓🍉Build Your Own Fruit Smoothie🍎🍏')
 
 import pandas
-my_fruit_list = pandas.read_csv("https://https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
